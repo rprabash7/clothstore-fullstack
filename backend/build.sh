467f954364474e1af10bb35a698ac6f8bb4d3d7e
Prabash <rprabash076@gmail.com>
2026-09-09 16:55:29 +0530

    Update build.sh

--- backend/build.sh
+++ backend/build.sh
@@ -2,5 +2,11 @@
 set -o errexit
 
 pip install -r requirements.txt
+
 python manage.py collectstatic --no-input
+
 python manage.py migrate
+
+if [ "$CREATE_SUPERUSER" = "True" ]; then
+  python manage.py createsuperuser --noinput || true
+fi
