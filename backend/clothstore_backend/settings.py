9eea13c50aea39f7f31383344ecb9a579dc41cee
Prabash <rprabash076@gmail.com>
2026-09-09 18:09:35 +0530

    Configure static files directory in settings
    Added STATICFILES_DIRS to settings for static files.

--- backend/clothstore_backend/settings.py
+++ backend/clothstore_backend/settings.py
@@ -104,6 +104,16 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
